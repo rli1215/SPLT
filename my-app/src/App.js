37c08d97b8a56e06f22bea9ca9c466309aee0621
Vanessa Lee <vlee81801@gmail.com>
2022-02-12 16:26:04 -0500

    Added item column to the UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,7 +16,7 @@ import {
 
 const Separator = () => <hr />;
 
-function Form() {
+function NameForm() {
   const [inputList, setInputList] = useState([{ name: ""}]);
 
   const handleInputChange = (e, i) => {
@@ -74,6 +74,65 @@ function Form() {
   );
 };
 
+function ItemForm() {
+  const [inputList, setInputList] = useState([{item: ""}]);
+
+  const handleInputChange = (e, i) => {
+    const { item, value } = e.target;
+    console.log(e.target);
+    const list = [...inputList];
+    list[i][item] = value;
+    setInputList(list);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputList);
+  };
+
+  // handle click event of the Remove button
+   const handleRemoveClick = index => {
+      const list = [...inputList];
+      list.splice(index, 1);
+      setInputList(list);
+   };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+      setInputList([...inputList, { item: "" }]);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid container alignItems="center" justify="center" direction="column">
+        {inputList.map((x, i) => {
+            return (
+              <div className="box">
+                <TextField
+                    id="item-input"
+                    name="item"
+                    label="Item"
+                    type="text"
+                    value={inputList[i]["name"]}
+                    onChange={e => handleInputChange(e, i)}
+                  />
+                <div className="btn-box">
+                  {inputList.length !== 1 && <button
+                    className="mr10"
+                    onClick={() => handleRemoveClick(i)}>Remove</button>}
+                </div>
+              </div>
+            );
+        })}
+        <Button id="addInputBtn" variant="contained" color="primary" type="add" onClick={handleAddClick}>+</Button>
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+      </Grid>
+    </form>
+  );
+};
+
 export default function App() {
     return (
         <div className="App">
@@ -81,7 +140,14 @@ export default function App() {
             <Separator />
             <h1>SPLT</h1>
             <Separator />
-            {<Form />}
+            <Grid container>
+              <Grid item xs={6}> 
+                {<NameForm />}
+              </Grid>
+              <Grid item xs={6}>
+                {<ItemForm />}
+              </Grid>
+            </Grid>
             {/* <Projects />
             <Separator /> */}
         </div>
