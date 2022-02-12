b85b9bf8c08c4576e6e9bf3a9af2f35f8debbbae
rli1215 <56896626+rli1215@users.noreply.github.com>
2022-02-12 15:13:40 -0500

    added react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,68 +1,78 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import {
+    Grid,
+    TextField,
+    FormControlLabel,
+    FormControl,
+    FormLabel,
+    RadioGroup,
+    Radio,
+    Select,
+    MenuItem,
+    Slider,
+    Button
+} from "@material-ui/core";
 
 const Separator = () => <hr />;
 
-const HeroImage = <img src="" alt="My Banner"/>
+function Form() {
+  const [inputList, setInputList] = useState([{ name: ""}]);
 
-const Projects = () => {
-    const projects = [];
+  const handleInputChange = (e, i) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[i][name] = value;
+    setInputList(list);
+  };
 
-    for (let i=1;i<=10;i+=1) {
-        projects.push(<a href="#" title={"Project"+i}>
-            <img 
-                src="" 
-                alt={'project image' + i}
-            />
-            </a>,
-            );
-    }
-    return (<React.Fragment>
-        <div>My Projects</div>
-        <div>{projects}</div>
-        </React.Fragment>);
-};
-
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+  const handleSubmit = (event) => {
     event.preventDefault();
-  }
+    console.log(inputList);
+  };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div class="form-group">
-            <table id="namesTable">
-                <tr>
-                    <th><label asp-for="Name" class="control-label" id="name1">
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                    </label></th>
-                </tr>
-                    <tr>
-                        <td><input type="text" value={this.state.value} onChange={this.handleChange} /></td>
-                    </tr>
-                    <span asp-validation-for="Name" class="text-danger"></span>
-            </table>
-        </div>
-        <input id="addInputBtn" type="button" value="+" />
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+  // handle click event of the Remove button
+   const handleRemoveClick = index => {
+      const list = [...inputList];
+      list.splice(index, 1);
+      setInputList(list);
+   };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+      setInputList([...inputList, { name: "" }]);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid container alignItems="center" justify="center" direction="column">
+        {inputList.map((x, i) => {
+            return (
+              <div className="box">
+                <TextField
+                    id="name-input"
+                    name="name"
+                    label="Name"
+                    type="text"
+                    value={inputList[i]["name"]}
+                    onChange={e => handleInputChange(e, i)}
+                  />
+                <div className="btn-box">
+                  {inputList.length !== 1 && <button
+                    className="mr10"
+                    onClick={() => handleRemoveClick(i)}>Remove</button>}
+                </div>
+              </div>
+            );
+        })}
+        <Button id="addInputBtn" variant="contained" color="primary" type="add" onClick={handleAddClick}>+</Button>
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+      </Grid>
+    </form>
+  );
+};
 
 export default function App() {
     return (
@@ -71,7 +81,7 @@ export default function App() {
             <Separator />
             <h1>SPLT</h1>
             <Separator />
-            {<NameForm />}
+            {<Form />}
             {/* <Projects />
             <Separator /> */}
         </div>
